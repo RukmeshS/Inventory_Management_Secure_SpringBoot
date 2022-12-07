15203f579587c67e3499bdec55349be4bd09a4c3
Prashantbag <prashant.bag14@gmail.com>
2022-12-07 17:04:51 +0530

    admin Dao added & Created Custom exception

--- src/main/java/com/ty/ims/inventory_prject_boot/dao/AdminDao.java
+++ src/main/java/com/ty/ims/inventory_prject_boot/dao/AdminDao.java
@@ -1,5 +1,48 @@
 package com.ty.ims.inventory_prject_boot.dao;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.ty.ims.inventory_prject_boot.dto.Admin;
+import com.ty.ims.inventory_prject_boot.repository.AdminRepository;
+
+@Repository
 public class AdminDao {
 
+	@Autowired
+	AdminRepository adminRepository;
+	
+	
+	public Admin saveAdmin(Admin admin) {
+		return adminRepository.save(admin);
+	}
+	
+	
+//	public List<Admin> getAllAdmin(){
+//		return adminRepository.getAdmins();
+//	}
+	
+	
+	public Admin getAdminById(int id) {
+		Optional<Admin> optional = adminRepository.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}else {
+			return null;
+		}
+	
+	}
+	
+	public Admin updateAdmin(Admin admin) {
+		return adminRepository.save(admin);
+	}
+	
+	public String deleteAdmin(int id) {
+		adminRepository.deleteById(id);
+		return "Deleted";
+	}
+	
 }
--- src/main/java/com/ty/ims/inventory_prject_boot/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/ty/ims/inventory_prject_boot/exception/ApplicationExceptionHandler.java
@@ -1,5 +1,65 @@
 package com.ty.ims.inventory_prject_boot.exception;
 
-public class ApplicationExceptionHandler {
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.ty.ims.inventory_prject_boot.util.ResponseStructure;
+
+@RestControllerAdvice
+public class ApplicationExceptionHandler  extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler
+	public ResponseEntity<ResponseStructure<String>> noSuchIdFoundExceptionHandler(NoSuchIdFoundException exception){
+		
+		ResponseStructure<String>  responseStructure = new ResponseStructure<String>();
+		responseStructure.setStatus(HttpStatus.NOT_FOUND.value());
+		responseStructure.setMessage("ID Not FOund");
+		responseStructure.setData(exception.getMessage());
+		
+		ResponseEntity<ResponseStructure<String>> responseEntity = new ResponseEntity<ResponseStructure<String>>(HttpStatus.NOT_FOUND);
+		
+		return responseEntity;
+		
+		
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<ObjectError> er = ex.getAllErrors();
+		
+		Map<String,String> map = new LinkedHashMap<String, String>();
+		
+		for (ObjectError objectError : er) {
+			
+			String message=objectError.getDefaultMessage();
+			String fieldname= ((FieldError)objectError).getField();
+			map.put(message, fieldname);
+		}
+		
+		
+		ResponseStructure<Map<String,String>>  responseStructure = new ResponseStructure<Map<String,String>>();
+		responseStructure.setStatus(HttpStatus.BAD_REQUEST.value());
+		responseStructure.setMessage("No Proper Input");
+		responseStructure.setData(map);
+		
+		
+		return new ResponseEntity<>(responseStructure,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+	
+	
 }
