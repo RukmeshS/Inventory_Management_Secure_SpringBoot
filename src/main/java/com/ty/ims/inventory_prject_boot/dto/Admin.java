89acdc1b1af3f92e996047ea9a9c2d7c5b8da42b
Prashantbag <prashant.bag14@gmail.com>
2022-12-07 12:01:52 +0530

    added admin dto ,response structure and application config

---
+++ src/main/java/com/ty/ims/inventory_prject_boot/dto/Admin.java
@@ -0,0 +1,27 @@
+package com.ty.ims.inventory_prject_boot.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	private String email;
+	@NotNull
+	private String password;
+	
+}
