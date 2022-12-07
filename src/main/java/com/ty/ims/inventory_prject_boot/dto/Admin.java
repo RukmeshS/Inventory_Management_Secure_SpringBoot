a8f9b06d48f52b1a9e9f0000ff76d8f1cafd0b15
Prashantbag <prashant.bag14@gmail.com>
2022-12-07 12:11:01 +0530

    Added admin repository

--- src/main/java/com/ty/ims/inventory_prject_boot/dto/Admin.java
+++ src/main/java/com/ty/ims/inventory_prject_boot/dto/Admin.java
@@ -12,7 +12,6 @@ import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
 @Entity
 public class Admin {
 
--- src/main/java/com/ty/ims/inventory_prject_boot/repository/AdminRepository.java
+++ src/main/java/com/ty/ims/inventory_prject_boot/repository/AdminRepository.java
@@ -1,5 +1,9 @@
 package com.ty.ims.inventory_prject_boot.repository;
 
-public class AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.ims.inventory_prject_boot.dto.Admin;
+
+public interface AdminRepository extends JpaRepository<Admin, Integer> {
 
 }
