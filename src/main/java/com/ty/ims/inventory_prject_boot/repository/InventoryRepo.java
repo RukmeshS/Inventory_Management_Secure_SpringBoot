e0bf662a225722f1ef747b2810af844c3d7a96d5
Rukmesh Srinivas <rukmesh.9@gmail.com>
2022-12-07 12:03:22 +0530

    Inventory,item-Dto Repo

---
+++ src/main/java/com/ty/ims/inventory_prject_boot/repository/InventoryRepo.java
@@ -0,0 +1,9 @@
+package com.ty.ims.inventory_prject_boot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ty.ims.inventory_prject_boot.dto.Inventory;
+
+public interface InventoryRepo extends JpaRepository<Inventory, Integer> {
+
+}
