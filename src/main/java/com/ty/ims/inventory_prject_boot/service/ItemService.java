997aa83865f06aeb8c9cf4ac68a2f23da97c68d0
Rukmesh Srinivas <rukmesh.9@gmail.com>
2022-12-10 10:07:06 +0530

    Inventory and item controller

--- src/main/java/com/ty/ims/inventory_prject_boot/service/ItemService.java
+++ src/main/java/com/ty/ims/inventory_prject_boot/service/ItemService.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.ty.ims.inventory_prject_boot.dao.InventoryDao;
 import com.ty.ims.inventory_prject_boot.dao.ItemDao;
+import com.ty.ims.inventory_prject_boot.dto.Inventory;
 import com.ty.ims.inventory_prject_boot.dto.Item;
 import com.ty.ims.inventory_prject_boot.exception.NoSuchIdFoundException;
 import com.ty.ims.inventory_prject_boot.util.ResponseStructure;
@@ -18,13 +20,20 @@ public class ItemService {
 	@Autowired
 	ItemDao dao;
 
-	public ResponseEntity<ResponseStructure<Item>> serviceSaveItem(Item item) {
-		ResponseStructure<Item> responseStructure = new ResponseStructure<Item>();
-		responseStructure.setStatus(HttpStatus.CREATED.value());
-		responseStructure.setMessage("Inventory Item Created");
-		responseStructure.setData(dao.saveItem(item));
-		return new ResponseEntity<ResponseStructure<Item>>(responseStructure, HttpStatus.CREATED);
+	@Autowired
+	InventoryDao invdao;
 
+	public ResponseEntity<ResponseStructure<Item>> serviceSaveItem(Item item, int id) {
+		ResponseStructure<Item> responseStructure = new ResponseStructure<Item>();
+		Optional<Inventory> optional = invdao.findInventorybyid(id);
+		if (optional.isPresent()) {
+			item.setInventory(optional.get());
+			responseStructure.setStatus(HttpStatus.CREATED.value());
+			responseStructure.setMessage("Inventory Item Created");
+			responseStructure.setData(dao.saveItem(item));
+			return new ResponseEntity<ResponseStructure<Item>>(responseStructure, HttpStatus.CREATED);
+		}
+		throw new NoSuchIdFoundException();
 	}
 
 	public ResponseEntity<ResponseStructure<Item>> serviceUpdateItem(Item item, int id) {
@@ -64,4 +73,14 @@ public class ItemService {
 		}
 		throw new NoSuchIdFoundException();
 	}
+
+	public ResponseEntity<ResponseStructure<Item>> serviceSaveItemandInventory(Item item, int id) {
+		ResponseStructure<Item> responseStructure = new ResponseStructure<Item>();
+		invdao.findInventorybyid(id);
+		responseStructure.setStatus(HttpStatus.CREATED.value());
+		responseStructure.setMessage("Inventory Item Created");
+		responseStructure.setData(dao.saveItem(item));
+		return new ResponseEntity<ResponseStructure<Item>>(responseStructure, HttpStatus.CREATED);
+	}
+
 }
